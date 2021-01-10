488b2520b734bb5ee9d0461affb3d7372bbb2828
yk7335 <60045080+yk7335@users.noreply.github.com>
2021-01-09 20:55:46 -0600

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,11 +1,8 @@
 <?php
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "yk7335@tamu.edu";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+<form action="mail.php" method="POST">
+<p>Name</p> <input type="text" name="name">
+<p>Email</p> <input type="text" name="email">
+<p>Message</p><textarea name="message" rows="6" cols="25"></textarea><br />
+<input type="submit" value="Send"><input type="reset" value="Clear">
+</form>
 ?>
