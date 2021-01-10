aa15a5ae07c137b403875ff3244220c267be4d9d
yk7335 <60045080+yk7335@users.noreply.github.com>
2021-01-10 03:35:35 -0600

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,16 +1,26 @@
 <?php
 $name = $_POST['name'];
 $email = $_POST['email'];
-$phone = $_POST['phone'];
-$call = $_POST['call'];
-$website = $_POST['website'];
-$priority = $_POST['priority'];
-$type = $_POST['type'];
 $message = $_POST['message'];
-$formcontent=" From: $name \n Phone: $phone \n Call Back: $call \n Website: $website \n Priority: $priority \n Type: $type \n Message: $message";
-$recipient = "yk7335@tamu.edu";
-$subject = "Contact Form";
+
+$email_from = 'yashkalyani456@gmail.com';
+
+$subject = "New Form Submission";
+
 $mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+
+$email_body = "User Name: $name.\n".
+                "User Email: $email.\n".
+                    "User Message: $message.\n";
+
+$recipient = "yk7335@tamu.edu";
+
+$headers = "Form: $email_from \r\n";
+
+$headers .= "Reply-To: $email \r\n";
+
+
+mail($recipient, $subject, $email_body, $headers) or die("Error!");
+header("Location: index.html");
 echo "Thank You!";
 ?>
