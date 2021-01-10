f5de466dfaf3384ec4a4b21a89b70f5b41f16c0d
yk7335 <60045080+yk7335@users.noreply.github.com>
2021-01-09 21:06:28 -0600

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,8 +1,13 @@
 <?php
 $name = $_POST['name'];
 $email = $_POST['email'];
+$phone = $_POST['phone'];
+$call = $_POST['call'];
+$website = $_POST['website'];
+$priority = $_POST['priority'];
+$type = $_POST['type'];
 $message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
+$formcontent=" From: $name \n Phone: $phone \n Call Back: $call \n Website: $website \n Priority: $priority \n Type: $type \n Message: $message";
 $recipient = "yk7335@tamu.edu";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
