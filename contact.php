ce168a31621d03174b2ebd6930fd64fd855410ab
yk7335 <60045080+yk7335@users.noreply.github.com>
2021-01-09 20:51:02 -0600

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,8 +1,11 @@
 <?php
-<form action="mail.php" method="POST">
-<p>Name</p> <input type="text" name="name">
-<p>Email</p> <input type="text" name="email">
-<p>Message</p><textarea name="message" rows="6" cols="25"></textarea><br />
-<input type="submit" value="Send"><input type="reset" value="Clear">
-</form>
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "emailaddress@here.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
 ?>
