884391108517ff46b4c1d2afacb70a30cc4e05f4
yk7335 <60045080+yk7335@users.noreply.github.com>
2021-01-13 23:40:38 -0600

    Add files via upload

--- contact.php
+++ contact.php
@@ -9,20 +9,20 @@
 
        if(empty($UserName) || empty($Email) || empty($Subject) || empty($Msg))
        {
-           header('location:index.php?error');
+           header('location:index.html?error');
        }
        else
        {
-           $to = "admin@onlineittuts.com";
+           $to = "yk7335@tamu.edu";
 
            if(mail($to,$Subject,$Msg,$Email))
            {
-               header("location:index.php?success");
+               header("location:index.html?success");
            }
        }
     }
     else
     {
-        header("location:index.php");
+        header("location:index.html");
     }
 ?>
